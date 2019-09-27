a9a67d4db67fe2051efbd0768eec7fc14860e802
Sayan Brahma <sbrahma0@gmail.com>
2019-09-27 19:23:10 -0400

    update test main file

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,11 @@
+/*!
+ *  \file  		Test file - main.cpp
+ *  \author 	part1: Sayan Brahma(driver) Akwashi a Obeng(navigator)
+ *  \author 	part2: Akwashi a Obeng(driver) Sayan Brahma(navigator)
+ *  \copyright  Sayan Brahma and Akwashi a Obeng
+ *  \
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
