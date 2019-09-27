0301486a82edf279a862037185df177644c0b06c
Sayan Brahma <sbrahma0@gmail.com>
2019-09-27 19:17:26 -0400

    adding the test file

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,29 @@
-#include <gtest/gtest.h>
+/*!
+ *  \brief 		This is the test file in the test folder which contains the test cases
+ *  \file  		Test file - test.cpp
+ *  \author 	part1: Sayan Brahma(driver) Akwashi a Obeng(navigator)
+ *  \copyright  Sayan Brahma and Akwashi a Obeng
+ *  \
+ */
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include <gtest/gtest.h>
+#include "../include/pid_controller.hpp"
+
+pidController ob(1.0, 0, 0, 1);
+pidController ob1(0.0, 0, 0, 1);
+
+/**
+ * @brief This is the kp test i.e., it is only a proportional controller with all the parameters set to 0
+ */
+
+TEST(kptest, should_pass) {
+  EXPECT_DOUBLE_EQ(ob.compute(12, 10), 2.0);
 }
+/**
+ * @brief This is the zero test i.e., when all the parameters are zero then the final value should be 0
+ */
+
+TEST(zerotest, should_pass) {
+  EXPECT_DOUBLE_EQ(ob1.compute(12, 10), 0.0);
+}
+
