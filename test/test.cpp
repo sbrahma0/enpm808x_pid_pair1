cfd87f7592ae53325fd843eb83be1e16a2cfbc33
Sayan Brahma <sbrahma0@gmail.com>
2019-09-27 22:33:32 -0400

    adding test conditions

--- test/test.cpp
+++ test/test.cpp
@@ -7,7 +7,43 @@
  */
 
 #include <gtest/gtest.h>
+#include "../include/pid_controller.hpp"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+pidController ob(1.0, 0.0, 0.0, 1.0);
+pidController ob1(0.0, 0.0, 0.0, 1.0);
+pidController ob2(-1.0, -1.0, -1.0, 1.0);
+pidController ob3(1.0, 1.0, 1.0, -1.0);
+
+
+
+/**
+ * @brief This is the kp test i.e., it is only a proportional controller with all the parameters set to 0
+ */
+
+TEST(kptest, should_pass) {
+  EXPECT_DOUBLE_EQ(ob.compute(12, 10), 2.0);
 }
+/**
+ * @brief This is the zero test i.e., when all the parameters are zero then the final value should be 0
+ */
+
+TEST(zerotest, should_pass) {
+  EXPECT_DOUBLE_EQ(ob1.compute(12, 10), 0.0);
+}
+
+/**
+ * @brief This is the negative test i.e., when all the parameters are -ve then the final value should be -ve
+ */
+
+TEST(negativetest, should_pass) {
+  EXPECT_DOUBLE_EQ(ob2.compute(12, 10), -6.0);
+}
+
+/**
+ * @brief This is the dttest i.e., when all the parameters are set to some value but the dt is less than 0
+ */
+
+TEST(dttest, should_pass) {
+  EXPECT_DOUBLE_EQ(ob3.compute(12, 10), 0.0);
+}
+
