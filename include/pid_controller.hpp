3b501c2dc0eb8d8bcaa240463b8cc9be68b72965
Sayan Brahma <sbrahma0@gmail.com>
2019-09-27 22:31:45 -0400

    incude update

--- include/pid_controller.hpp
+++ include/pid_controller.hpp
@@ -15,7 +15,7 @@ class pidController {
 	 *	@brief contains the private data members
 	 */
  private:
-    double kp = 0.0, ki = 0.0, kd = 0.0, dt = 1.0;
+    double kp = 0.0, ki = 0.0, kd = 0.0, dt = 1.0;  // default values
 	/**
 	 *	@brief contains all the public methods/functions
 	 */
@@ -25,7 +25,7 @@ class pidController {
 	 */
     pidController();
 	/**
-	 * @brief constructor to initialize the default values
+	 * @brief constructor to initialize the user input values
 	 * @param kp proportional constant
 	 * @param ki integral constant
 	 * @param kd differentiation constant
@@ -33,11 +33,11 @@ class pidController {
 	 */
     pidController(double kp, double ki, double kd, double dt);
 	/**
-	 *
+	 * @brief A destructor which is called after the constructor has been used
 	 */
     ~pidController();
 	/**
-	 *
+	 * @brief This function calculates the error and new velocity based on the setvalue and the current velocity
 	 */
     double compute(double setValue, double currentValue);
 };
